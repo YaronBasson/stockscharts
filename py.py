a63c7c598eeb19713f092959dea6ad38b71a32d8
Yaron Basson <yaron.base@gmail.com>
2026-04-10 01:09:51 +0300

    commit

--- py.py
+++ py.py
@@ -1,11 +1,12 @@
-import requests;
-KEY='asi2MBtO6ryKsZtstBptDMfJwv2Fb34b';
+import os
+import requests
+
+KEY = os.getenv("POLYGON_API_KEY", "")
+
 #r=requests.get('https://api.polygon.io/v2/aggs/ticker/MNQ/range/15/minute/2026-04-01/2026-04-03',params={'sort':'asc','limit':5,'apiKey':KEY},timeout=10);
 #print(r.status_code, r.text[:300])
 
-
 #r=requests.get('https://api.polygon.io/v3/reference/tickers',params={'search':'MNQ','market':'futures','apiKey':KEY},timeout=10)
-#r=requests.get('https://api.polygon.io/v3/reference/tickers',params={'search':'MNQ','market':'futures','apiKey':'asi2MBtO6ryKsZtstBptDMfJwv2Fb34b'},timeout=10)
 #print(r.status_code)
 #print(r.text[:600])
 r=requests.get('https://api.polygon.io/v3/reference/tickers',params={'search':'Micro E-mini Nasdaq','apiKey':KEY},timeout=10)
