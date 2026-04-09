0b4d7b341fc99b236c1553c12d09c641baa30744
Yaron Basson <yaron.base@gmail.com>
2026-04-10 01:05:01 +0300

    commit

--- web_app.py
+++ web_app.py
@@ -533,4 +533,4 @@ def api_scan():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8080, use_reloader=False)
+    app.run(debug=False, host="127.0.0.1", port=8080, use_reloader=False)
